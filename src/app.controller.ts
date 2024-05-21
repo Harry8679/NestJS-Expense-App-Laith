1b358f3b0d4f4adec4dcc37f234d2e9350e6155a
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-21 11:22:59 +0200

    Updating the Routh Path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
+  @Get('report/income')
   getAllIncomeReports() {
-    return {};
+    return [];
   }
 }
