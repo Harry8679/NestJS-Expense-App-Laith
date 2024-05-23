11fd9d5fdba12d027664835750d03f90c9dd7eff
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-23 14:39:44 +0200

    deleteReport Service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line prettier/prettier
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { data, ReportType } from './data';
+import { ReportType } from './data';
 // import { v4 as uuid } from 'uuid';
 import { AppService } from './app.service';
 
@@ -47,12 +47,6 @@ export class AppController {
 
   @Delete(':id')
   deleteReport(@Param('id') id: string) {
-    const reportIndex = data.report.findIndex((report) => report.id === id);
-
-    if (reportIndex === -1) return;
-
-    data.report.splice(reportIndex, 1);
-
-    return;
+    return this.appService.deleteReport(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -52,4 +52,14 @@ export class AppService {
 
     return data.report[reportIndex];
   }
+
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex === -1) return;
+
+    data.report.splice(reportIndex, 1);
+
+    return;
+  }
 }
